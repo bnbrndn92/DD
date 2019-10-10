9b12f98a74c0c725343ab45f08265036db1cfce0
JordenPowley <Jorden.Powley.WebDev@gmail.com>
2019-10-10 12:16:13 +0100

    Added client update features.

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -82,4 +82,64 @@ class ClientController extends Controller
             "location" => "/management/client/" . $client->getId()
         ],201);
     }
+
+    /**
+     * editClient()
+     *
+     * Process:
+     *
+     * @Route("/client/{id}/edit", name="client-edit", requirements={"id"="\d+"})
+     *
+     * @param Request $request
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function editClient (Request $request, int $id) :response
+    {
+        // Check that API access is allowed
+        $this->checkApiAccess($request);
+
+        // Check that POST data has been provided & decode
+        $data = $this->checkApiJson($request);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $client = $entityManager->getRepository(Client::class)->findOneBy(["id" => intval($id)]);
+
+        if (empty($client)) {
+            return new JsonResponse([
+                "success" => false,
+                "message" => "No client found",
+            ],404);
+        }
+
+        // Check for updated fields
+        if (array_key_exists("client_name", $data)) {
+            // Compare the old and the new
+            if (trim($data['client_name']) !== $client->getName()) {
+                // Update the client name
+                $client->setName(trim($data['client_name']));
+                $client->generateSafeName($client->getName());
+
+                // Check if a client exists already with this safe name
+                $existingClient = $entityManager->getRepository(Client::class)->findByClientSafeName($client->getSafeName());
+
+                if (!empty($existingClient)) {
+                    // Client with tha safe name already exists -> return response
+                    return new JsonResponse([
+                        "success" => false,
+                        "message" => "A client with that name already exists.",
+                    ],409);
+                }
+            }
+        }
+
+        $entityManager->flush();
+
+        return new JsonResponse([
+            "success" => true,
+            "message" => "Client updated",
+            "location" => "/management/client/" . $client->getId()
+        ],200);
+    }
 }
--- src/Controller/ManagementController.php
+++ src/Controller/ManagementController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Client;
+use App\Entity\Frontend;
+use App\Entity\Service;
+use App\Repository\ClientRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ManagementController extends Controller
 {
@@ -52,19 +56,98 @@ class ManagementController extends Controller
     /**
      * viewClient()
      *
+     * TODO - Include auth checks
+     *
      * @Route("/management/client/{id}", name="management-view-client", requirements={"id"="\d+"})
      *
+     * @param Request $request
      * @param int $id
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function viewClient (int $id)
+    public function viewClient (Request $request, int $id)
     {
+        /** @var ClientRepository $clientRepo */
+        $clientRepo = $this->getDoctrine()
+            ->getRepository(Client::class);
+
+        $client = $clientRepo->findOneBy([
+            "id" => intval($id),
+        ]);
+
+        if (empty($client)) {
+            return $this->render('pages/error.html.twig', [
+                "pageTitle" => "Error",
+                "pageDescription" => "Error page",
+                "pageKeywords" => "management, client, error",
+                "bodyClass" => "error-page",
+                "message" => "No client data found",
+            ]);
+        }
+
+        // Attempt to pull out the services
+        $serviceRepo = $this->getDoctrine()
+            ->getRepository(Service::class);
+
+        $services = $serviceRepo->findBy([
+            "client_id" => $client->getId(),
+        ]);
+
+        // Attempt to pull out the frontends
+        $frontendRepo = $this->getDoctrine()
+            ->getRepository(Frontend::class);
+
+        $frontends = $frontendRepo->findBy([
+            "client_id" => $client->getId(),
+        ]);
+
         return $this->render('pages/management/view-client.html.twig', [
-            "pageTitle" => "Client Name",
+            "pageTitle" => ucwords($client->getName()),
             "pageDescription" => "Client page",
             "pageKeywords" => "management, client",
-            "bodyClass" => "management-client-page"
+            "bodyClass" => "management-client-page",
+            "client" => $client,
+            "services" => (!empty($services)) ? $services : null,
+            "frontends" => (!empty($frontends)) ? $frontends : null,
+        ]);
+    }
+
+    /**
+     * editClient()
+     *
+     * @Route("/management/client/{id}/edit", name="management-edit-client", requirements={"id"="\d+"})
+     *
+     * @param Request $request
+     * @param int $id
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editClient (Request $request, int $id)
+    {
+        /** @var ClientRepository $clientRepo */
+        $clientRepo = $this->getDoctrine()
+            ->getRepository(Client::class);
+
+        $client = $clientRepo->findOneBy([
+            "id" => intval($id),
+        ]);
+
+        if (empty($client)) {
+            return $this->render('pages/error.html.twig', [
+                "pageTitle" => "Error",
+                "pageDescription" => "Error page",
+                "pageKeywords" => "management, client, error",
+                "bodyClass" => "error-page",
+                "message" => "No client data found",
+            ]);
+        }
+
+        return $this->render('pages/management/edit-client.html.twig', [
+            "pageTitle" => "Edit: " . ucwords($client->getName()),
+            "pageDescription" => "Edit client page",
+            "pageKeywords" => "management, client",
+            "bodyClass" => "management-edit-client-page",
+            "client" => $client,
         ]);
     }
 
@@ -84,6 +167,25 @@ class ManagementController extends Controller
             "pageDescription" => "Service page",
             "pageKeywords" => "management, service",
             "bodyClass" => "management-service-page"
+        ]);
+    }
+
+    /**
+     * viewFrontend()
+     *
+     * @Route("/management/frontend/{id}", name="management-view-frontend", requirements={"id"="\d+"})
+     *
+     * @param int $id
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function viewFrontend (int $id)
+    {
+        return $this->render('pages/management/view-frontend.html.twig', [
+            "pageTitle" => "Frontend Name",
+            "pageDescription" => "frontend page",
+            "pageKeywords" => "management, frontend",
+            "bodyClass" => "management-frontend-page"
         ]);
     }
 }
